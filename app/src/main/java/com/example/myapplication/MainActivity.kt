6df865002adc707d38171e6dd122cb0b8d295096
luka <luka.janjgava.1@btu.edu.ge>
2022-09-24 18:02:01 +0400

    new feature

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        println("new feature added")
 
     }
 }
